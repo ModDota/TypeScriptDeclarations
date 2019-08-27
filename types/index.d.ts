04cf27040f3e3286ccb6b3945cbbbc707ec059a6
ark120202 <ark120202@gmail.com>
2019-08-27 15:37:03 +0500

    feat: add `json` global library type

--- types/index.d.ts
+++ types/index.d.ts
@@ -114,3 +114,44 @@ declare function Dynamic_Wrap<
       : never;
   }[keyof T]
 >(this: void, context: T, name: K): T[K];
+
+declare namespace json {
+  interface EncodeOptions {
+    /**
+     * When indent is set, the created string will contain newlines and
+     * indentations. Otherwise it will be one long line.
+     */
+    indent?: boolean;
+
+    /**
+     * An array to specify the ordering of keys in the encoded output. If an
+     * object has keys which are not in this array they are written after the
+     * sorted keys.
+     */
+    keyorder?: (keyof any)[];
+
+    /**
+     * The initial level of indentation used when indent is set. For each level
+     * two spaces are added.
+     * @default 0
+     */
+    level?: number;
+  }
+}
+
+declare const json: {
+  readonly version: string;
+  readonly null: unique symbol;
+
+  /** @tupleReturn */
+  decode(
+    this: void,
+    string: string,
+    position?: number,
+    nullValue?: any,
+    objectMeta?: LuaMetatable<object>,
+    arrayMeta?: LuaMetatable<any[]>,
+  ): [any, number] | [undefined, number, string];
+
+  encode(this: void, object: any, options: json.EncodeOptions): string;
+};

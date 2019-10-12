8ee37303b01fd904ff8e6c8f73eee57df92a3617
ark120202 <ark120202@gmail.com>
2019-10-13 01:18:54 +0500

    feat: add `Object.observe` type

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,3 +8,28 @@ type EntityID = number & { readonly __tag__: 'EntityID' };
 type ItemID = number & { readonly __tag__: 'ItemID' };
 type ParticleID = number & { readonly __tag__: 'ParticleID' };
 type ScheduleID = number & { readonly __tag__: 'ScheduleID' };
+
+type ObserveEventType = ObserveChange<any>['type'];
+
+type ObserveChange<T> =
+    | {
+          type: 'add' | 'reconfigure' | 'setPrototype' | 'preventExtensions';
+          object: T;
+          name: string;
+      }
+    | {
+          type: 'update' | 'delete';
+          oldValue: any;
+          object: T;
+          name: string;
+      };
+
+interface ObjectConstructor {
+    getNotifier(obj: object): any;
+    observe<T extends object>(
+        obj: T,
+        callback: (changes: ObserveChange<T>[]) => void,
+        acceptList?: ObserveEventType[],
+    ): T;
+    unobserve<T extends object>(obj: T, callback: (changes: ObserveChange<T>[]) => void): T;
+}

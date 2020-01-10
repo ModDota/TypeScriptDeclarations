9fa92c4ea31c56a9c5e7a0e3fabb7a444460d94f
ark120202 <ark120202@gmail.com>
2020-01-10 11:07:41 +0500

    feat: take enum data from `dota-data`

--- build/generate/enums.ts
+++ build/generate/enums.ts
@@ -1,37 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { emit } from './utils';
-
-interface EnumDeclaration {
-    name: string;
-    members: EnumMember[];
-}
-
-interface EnumMember {
-    name: string;
-    value: number;
-}
-
-// TODO: Move to DotaTracking
-function parseDump() {
-    // Output of `cl_panorama_script_help *` command
-    const dump = fs.readFileSync(path.join(__dirname, 'cl_panorama_script_help'), 'utf8');
-    return dump.split('\n\n').map(
-        (group): EnumDeclaration => {
-            const enumName = group.match(/Enumeration '(.+?)'/)![1];
-            const members = [...group.matchAll(/^\t.+\.(.+?) = (\d+)/gm)].map(
-                ([, name, value]): EnumMember => ({ name, value: Number(value) }),
-            );
-
-            return { name: enumName, members };
-        },
-    );
-}
+import enums from 'dota-data/files/panorama/enums';
+import { emit, withDescription } from './utils';
 
 export const generatedEnums = emit(
-    parseDump()
+    enums
         .map(({ name: enumName, members }) => {
-            const memberTypes = members.map(({ name, value }) => `${name} = ${value}`).join(',');
+            const memberTypes = members
+                .map(({ name, description, value }) => withDescription(`${name} = ${value}`, description))
+                .join(',\n');
+
             return `declare enum ${enumName} {${memberTypes}}`;
         })
         .join('\n\n'),

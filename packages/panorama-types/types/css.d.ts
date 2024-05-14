1db9859fe42f1de67a06d6a671140a5db61512a7
LockingReal <1191515088@qq.com>
2024-05-14 22:57:06 +0800

    chore(css): add two types of attribute values for overflow

--- packages/panorama-types/types/css.d.ts
+++ packages/panorama-types/types/css.d.ts
@@ -527,7 +527,7 @@ interface VCSSStyleDeclaration {
      * overflow: squish squish; // squishes contents in horizontal and vertical directions
      * overflow: squish scroll; // scrolls contents in the Y direction
      */
-    overflow: 'squish' | 'clip' | 'scroll' | null;
+    overflow: 'squish' | 'clip' | 'scroll' | 'squish squish' | 'squish clip ' | 'squish scroll' | 'clip clip' | 'clip squish' | 'clip scroll' | 'scroll scroll' | 'scroll squish' | 'scroll clip' | null;
 
     padding: string | null;
     paddingBottom: string | null;
